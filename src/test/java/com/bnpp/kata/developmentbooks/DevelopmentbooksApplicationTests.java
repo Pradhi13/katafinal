968f3c0ea2a175445bfb098ce4c6481464115ea5
2025-DEV1-015 <thangarajpradheep@gmail.com>
2026-03-18 10:31:09 +0530

    chore: initial setup for java spring boot book pricing project

---
+++ src/test/java/com/bnpp/kata/developmentbooks/DevelopmentbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bnpp.kata.developmentbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopmentbooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

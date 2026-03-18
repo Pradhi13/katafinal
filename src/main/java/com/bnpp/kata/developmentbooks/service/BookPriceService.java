d0c65f33a39e03290d95c9847fb48ac4960f8a7a
2025-DEV1-015 <thangarajpradheep@gmail.com>
2026-03-18 10:43:28 +0530

    feat: add basic book price service and single book test

---
+++ src/main/java/com/bnpp/kata/developmentbooks/service/BookPriceService.java
@@ -0,0 +1,11 @@
+package com.bnpp.kata.developmentbooks.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookPriceService {
+
+    public double calculateBookPrice(String book){
+        return 50.0;
+    }
+}
---
+++ src/test/java/com/bnpp/kata/developmentbooks/service/BookPriceServiceTest.java
@@ -0,0 +1,24 @@
+package com.bnpp.kata.developmentbooks.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class BookPriceServiceTest {
+
+    private BookPriceService bookPriceService;
+
+    @BeforeEach
+    public void setup(){
+        bookPriceService=new BookPriceService();
+    }
+
+    @Test
+    @DisplayName("should return base price for a single book without discount")
+    public void calculateSingleBookPrice(){
+        double price = bookPriceService.calculateBookPrice("Clean Code");
+        assertEquals(50.0,price);
+    }
+}
